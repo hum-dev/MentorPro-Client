45229a00a1cb4c0349b557700086a897ce9402ea
hum-dev <humphreymuriungi@outlook.com>
2023-07-02 12:16:20 +0300

    prof err fixed

--- src/components/Navigations/Nav.style.jsx
+++ src/components/Navigations/Nav.style.jsx
@@ -80,9 +80,9 @@ font-size : 1.8rem;
     height: 70px;
 }
 
-& .profile{
-    /* width: 100%; */
-    /* height: 100%; */
+& .profile img{
+     width: 100%; 
+    height: 100%; 
     object-fit: cover;
     border-radius: 50%;
     border: 1px solid var(--color-white);
